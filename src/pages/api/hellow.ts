916533a4b4048efd9bb3cbcc41a2d6ca8ef721b5
ryota-09 <n.ryo950911@gmail.com>
2023-04-12 17:14:40 +0900

    デバッグ用のオブジェクトを追加

--- src/pages/api/hellow.ts
+++ src/pages/api/hellow.ts
@@ -5,7 +5,12 @@ export const config = {
 };
 
 export default async function handler(req: NextApiRequest) {
-  return new Response("ハロー ワールド",{
+  const dummy = {
+    deepl: process.env.NEXT_PUBLIC_DEEPL_KEY,
+    chatgpt: process.env.NEXT_PUBLIC_CHARGPT_KEY,
+  };
+
+  return new Response(`${dummy}`, {
     status: 200,
     headers: {
       "content-type": "application/json",
