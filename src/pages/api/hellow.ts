6ba30544668133a3b79a65cf67bbe8f0a87a8a76
ryota-09 <n.ryo950911@gmail.com>
2023-04-17 08:38:08 +0900

    デバッグ用のapiを作成

--- src/pages/api/hellow.ts
+++ src/pages/api/hellow.ts
@@ -5,23 +5,36 @@ export const config = {
 };
 
 export default async function handler(req: NextApiRequest) {
-  const apiUrl = `https://api-free.deepl.com/v2/translate`;
-  const params = new URLSearchParams({
-    auth_key: process.env.NEXT_PUBLIC_DEEPL_KEY ?? "",
-    text: "りんご",
-    target_lang: "en",
-  });
-  const deepLresponse = await fetch(`${apiUrl}?${params.toString()}`, {
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    method: "POST",
-    mode: "cors",
-    body: JSON.stringify({ text: "りんご", target_lang: "en" }),
-  });
-  const deepLdata = await deepLresponse.json();
+  // const apiUrl = `https://api-free.deepl.com/v2/translate`;
+  // const params = new URLSearchParams({
+  //   auth_key: process.env.NEXT_PUBLIC_DEEPL_KEY ?? "",
+  //   text: "りんご",
+  //   target_lang: "en",
+  // });
+  // const deepLresponse = await fetch(`${apiUrl}?${params.toString()}`, {
+  //   headers: {
+  //     "Content-Type": "application/x-www-form-urlencoded",
+  //   },
+  //   method: "POST",
+  //   mode: "cors",
+  //   body: JSON.stringify({ text: "りんご", target_lang: "en" }),
+  // });
+  // const deepLdata = await deepLresponse.json();
 
-  return new Response(JSON.stringify(deepLdata.translations[0].text), {
+  // return new Response(JSON.stringify(deepLdata.translations[0].text), {
+  //   status: 200,
+  //   headers: {
+  //     "content-type": "application/json",
+  //   },
+  // });
+  const apiUrl = `https://jsonplaceholder.typicode.com/posts`;
+  const response = await fetch(apiUrl, {
+    method: "GET",
+    mode: "cors",
+  });
+  const data = await response.json();
+
+  return new Response(JSON.stringify(data), {
     status: 200,
     headers: {
       "content-type": "application/json",
